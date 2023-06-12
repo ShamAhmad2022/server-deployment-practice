2177b80b234c3909e558da4a682ae61e1bb136b2
ShamAhmad2022 <shamahmadaljalam@gmail.com>
2023-06-12 16:30:44 +0000

    created the main structure for the server

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+
+const pageNotFoundHandler = require('./Middleware/404');
+const serverError = require('./Middleware/500')
+
+const app = express();
+app.use(cors());
+
+
+app.get('/', homeHandler);
+app.get('/error', intentionalErrorsHandler);
+
+app.use('*', pageNotFoundHandler)
+app.use(serverError)
+
+
+function homeHandler (req, res) {
+    res.status(200).json({
+        code:200,
+        message: 'Welcome to the Home page :)'
+    })
+}
+
+function intentionalErrorsHandler(req, res, next) {
+    req.body={
+        test: 'test'
+    }
+    next(message = {test : 'test bla'})
+}
+
+function start(port){
+    app.listen(port, ()=> console.log(`Up and running on port ${port}`))
+}
+
+module.exports = {
+    app,
+    start
+};
